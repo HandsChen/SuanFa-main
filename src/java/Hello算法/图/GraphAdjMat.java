2e928fc01578d2002feaed8d6d36794069209cc3
chen <946586523@qq.com>
2025-01-26 10:55:53 +0800

    提交

---
+++ src/java/Hello算法/图/GraphAdjMat.java
@@ -0,0 +1,13 @@
+package Hello算法.图;
+
+import java.util.ArrayList;
+
+/**
+ * 使用邻接矩阵表示图
+ */
+public class GraphAdjMat {
+    public static void main(String[] args) {
+        ArrayList<Integer> integers = new ArrayList<>();
+        integers.add(0,1);
+    }
+}
--- src/java/Hello算法/堆/HeapBase.java
+++ src/java/Hello算法/堆/HeapBase.java
@@ -3,7 +3,10 @@ package Hello算法.堆;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
-public class Heap {
+/**
+ * 堆基础
+ */
+public class HeapBase {
     //初始化
     private static final Queue<Integer> minHeap = new PriorityQueue<>();
     private static final Queue<Integer> maxHeap = new PriorityQueue<>((a, b) -> b - a);
@@ -19,5 +22,20 @@ public class Heap {
         /*获取栈顶元素*/
         Integer peek = maxHeap.peek();
         System.out.println("大顶堆的栈顶元素为 = " + peek);
+
+        /*堆顶元素出堆*/
+        //出堆元素会形成一个从大到小的序列
+        peek =maxHeap.poll(); //5
+        peek =maxHeap.poll(); //4
+        peek =maxHeap.poll(); //3
+        peek =maxHeap.poll(); //2
+        peek =maxHeap.poll(); //1
+        /*获取堆大小*/
+        int size= maxHeap.size();
+        /*判断堆是否为空*/
+        boolean isEmpty = maxHeap.isEmpty();
+        System.out.println("isEmpty = " + isEmpty);
+        /* 输入列表并建堆 */
+//        minHeap = new PriorityQueue<>(Arrays.asList(1, 3, 2, 5, 4));
     }
 }
